9ab18e70e4ec2807b7e30b584f2b48b5098a7a13
Patryk <patryk.stachura@stanley-p.pl>
2019-04-18 00:03:49 +0200

    Add animation ( Animate.css)
    In nav sections add'ed wellloked animation using jQuery

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -69,5 +69,34 @@ $('a[href*="#"]')
         });
       }
     }
-  });
+
+/*   ----------------------   */
+/* ---- Scroll Animation ---- */
+/*   ----------------------   */    
+
+    
+    $('.js--waypoint-1').waypoint(function(direction){
+        $('.js--waypoint-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+     $('.js--waypoint-2').waypoint(function(direction){
+        $('.js--waypoint-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--waypoint-3').waypoint(function(direction){
+        $('.js--waypoint-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+      $('.js--waypoint-4').waypoint(function(direction){
+        $('.js--waypoint-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+});
 
